fef54ba401c6c01ad238be7c4d025755e84832fd
ChandaranaNamita <namita.chandarana@umusic.com>
2019-03-20 17:17:57 +0000

    Code

--- src/Page10.java
+++ src/Page10.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Page10 {
+    public static void main(String[] args) {
+        int x = 3;
+        String name = "Dirk";
+        x = x * 17;
+        System.out.println("x is " + x);
+        double d = Math.random();
+        System.out.println("d is " +d);
+    }
 }
