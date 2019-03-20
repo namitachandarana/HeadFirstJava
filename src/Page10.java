06e15fe390913eb7e85f41f4a5b38051b7ee36e7
ChandaranaNamita <namita.chandarana@umusic.com>
2019-03-20 17:24:58 +0000

    Finishing page 10

--- src/Page10.java
+++ src/Page10.java
@@ -6,5 +6,27 @@ public class Page10 {
         System.out.println("x is " + x);
         double d = Math.random();
         System.out.println("d is " +d);
+
+        while (x > 12)
+        {
+            x = x -1;
+        }
+
+        for (x=0; x < 10; x = x+1)
+        {
+            System.out.println("x is now" +x);
+        }
+
+        if(x == 10) {
+            System.out.println("x must be 10");
+        }else{
+            System.out.println("x isnt 10");
+        }
+
+        if ((x > 3) & (name.equals("Dirk")))
+        {
+            System.out.println("Gently");
+        }
+        System.out.println("This line runs no matter what");
     }
 }
